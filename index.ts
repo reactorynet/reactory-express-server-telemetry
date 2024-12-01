2cad7ba761e6e2ad6ef8e7b5ac3bb7d3c564b8e0
Werner Weber <wweber@worldremit.com>
2024-12-01 04:10:57 +0200

    RSE-23: Configure OTEL api / sdk

---
+++ index.ts
@@ -0,0 +1,23 @@
+const ReactoryTelemetryModule: Reactory.Server.IReactoryModule = {
+  name: "ReactoryTelemetry",
+  version: "1.0.0",
+  nameSpace: "reactory",
+  priority: 1,
+  cli: [],
+  clientPlugins: [],
+  dependencies: [],
+  description: "A module that provides telemetry services for Reactory",
+  forms: [],
+  graphDefinitions: null,
+  grpc: null,
+  id: "reactory.ReactoryTelemetry@1.0.0",
+  models: [],
+  passportProviders: [],
+  pdfs: [],
+  routes: {},
+  services: [],
+  translations: [],
+  workflows: []
+};
+
+export default ReactoryTelemetryModule;
---
+++ reactory.inst.otlp.ts
@@ -0,0 +1,18 @@
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+
+const consumer = new NodeSDK({
+  serviceName: process.env.REACTORY_SERVICE_NAME || 'reactory-server',
+  traceExporter: new OTLPTraceExporter({
+    url: `${process.env.JAEGER_URL || 'http://127.0.0.1:4318'}/v1/traces`
+  }),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter({
+      url: `${process.env.JAEGER_URL || 'http://127.0.0.1:4318'}/v1/metrics`
+    }),
+  }),
+});
+
+consumer.start();
